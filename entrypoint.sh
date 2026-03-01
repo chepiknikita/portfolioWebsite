685ac506a6e8a9a2466619826ae7b669f30fa994
Nikita <TsarevNF@yandex.ru>
2026-03-01 23:47:12 +0500

    Hot fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-node node_modules/prisma/bin/prisma.js migrate deploy
+node node_modules/prisma/build/index.js migrate deploy
 
 echo "Starting application..."
 exec node .output/server/index.mjs
